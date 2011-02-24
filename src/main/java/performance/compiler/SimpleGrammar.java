302086464e9c656c83c27cd608c3becc40a18919
Juan Cruz Nores <juancn@gmail.com>
2011-02-24 15:52:36 -0300

    Checking for additional tokens.

--- src/main/java/performance/compiler/SimpleGrammar.java
+++ src/main/java/performance/compiler/SimpleGrammar.java
@@ -8,52 +8,54 @@ import performance.parser.PrefixParser;
 import performance.parser.Token;
 import performance.parser.ast.Expr;
 
+import static performance.compiler.TokenType.*;
+
 public final class SimpleGrammar
     extends Grammar<TokenType> {
     private SimpleGrammar() {
-        infix(TokenType.LAND, 30);
-        infix(TokenType.LOR, 30);
+        infix(LAND, 30);
+        infix(LOR, 30);
 
-        infix(TokenType.LT, 40);
-        infix(TokenType.GT, 40);
-        infix(TokenType.LE, 40);
-        infix(TokenType.GE, 40);
+        infix(LT, 40);
+        infix(GT, 40);
+        infix(LE, 40);
+        infix(GE, 40);
 
-        infix(TokenType.PLUS, 50);
-        infix(TokenType.MINUS, 50);
+        infix(PLUS, 50);
+        infix(MINUS, 50);
 
-        infix(TokenType.MUL, 60);
-        infix(TokenType.DIV, 60);
+        infix(MUL, 60);
+        infix(DIV, 60);
 
-        unary(TokenType.MINUS, 70);
-        unary(TokenType.NOT, 70);
+        unary(MINUS, 70);
+        unary(NOT, 70);
 
-        infix(TokenType.DOT, 80);
+        infix(DOT, 80);
 
-        prefix(TokenType.LPAREN, new PrefixParser<TokenType>(){
+        prefix(LPAREN, new PrefixParser<TokenType>(){
             @Override
             public Expr<TokenType> parse(PrattParser<TokenType> prattParser, Token<TokenType> tokenTypeToken) throws ParseException {
                 Expr<TokenType> result = prattParser.parseExpression(0);
-                if(prattParser.consume().getType() != TokenType.RPAREN) {
+                if(prattParser.consume().getType() != RPAREN) {
                     throw new ParseException("Unmatched right parenthesis");
                 }
                 return result;
             }
         });
 
-        constant(TokenType.INT_LITERAL);
-        constant(TokenType.LONG_LITERAL);
-        constant(TokenType.FLOAT_LITERAL);
-        constant(TokenType.DOUBLE_LITERAL);
-        constant(TokenType.STRING);
-        constant(TokenType.ID);
+        constant(INT_LITERAL);
+        constant(LONG_LITERAL);
+        constant(FLOAT_LITERAL);
+        constant(DOUBLE_LITERAL);
+        constant(STRING);
+        constant(ID);
     }
 
 
     public static final SimpleGrammar INSTANCE = new SimpleGrammar();
 
     public static void main(String[] args) throws ParseException {
-        String text = "Test.bah < 9*Test.buu";
+        String text = "Test.bah < 9*Test.buu ";
         Expr expr = parse(text);
         System.out.println("expr = " + expr);
     }
@@ -61,7 +63,11 @@ public final class SimpleGrammar
     public static Expr<TokenType> parse(final String text) throws ParseException {
         final Lexer<TokenType> lexer = new JavaLexer(text, 0 , text.length());
         final PrattParser<TokenType> prattParser = new PrattParser<TokenType>(INSTANCE, lexer);
-        return prattParser.parseExpression(0);
+        final Expr<TokenType> expr = prattParser.parseExpression(0);
+        if(prattParser.current().getType() != EOF) {
+            throw new ParseException("Unexpected token: " + prattParser.current());
+        }
+        return expr;
     }
 
 }
