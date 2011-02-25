cafa727ce0142ac5c3f1a0b4439d0e4c9dcc5c93
Juan Cruz Nores <juancn@gmail.com>
2011-02-25 18:24:29 -0300

    Retransforming of java.util and java.io

--- src/main/java/performance/transformer/PerformanceAgent.java
+++ src/main/java/performance/transformer/PerformanceAgent.java
@@ -4,10 +4,15 @@ import org.objectweb.asm.ClassReader;
 import org.objectweb.asm.ClassVisitor;
 import org.objectweb.asm.ClassWriter;
 
+import java.io.IOException;
+import java.io.InputStream;
 import java.lang.instrument.ClassFileTransformer;
 import java.lang.instrument.IllegalClassFormatException;
 import java.lang.instrument.Instrumentation;
+import java.lang.instrument.UnmodifiableClassException;
 import java.security.ProtectionDomain;
+import java.util.HashSet;
+import java.util.Set;
 
 @SuppressWarnings({"UnusedDeclaration"})
 public class PerformanceAgent
@@ -41,8 +46,40 @@ public class PerformanceAgent
         return bytes;
     }
 
-    public static void premain(String agentArgs, Instrumentation inst) {
-        inst.addTransformer(new PerformanceAgent());
+    public static void premain(String agentArgs, Instrumentation inst)
+            throws ClassNotFoundException, UnmodifiableClassException, IOException {
+        if(inst.isRetransformClassesSupported()) {
+            inst.addTransformer(new PerformanceAgent(), true);
+            for (Class aClass : inst.getAllLoadedClasses()) {
+                if(inst.isModifiableClass(aClass) && !aClass.isInterface()
+                        && include(aClass)
+                        && !skip.contains(aClass.getName())) {
+                    inst.retransformClasses(aClass);
+                }
+            }
+        } else {
+            inst.addTransformer(new PerformanceAgent(), false);
+            System.err.println("*** WARNING: JVM does not support retransforming. Classes already loaded will not be instrumented.");
+        }
+
+
     }
+
+    private static boolean include(Class aClass) {
+        final String name = aClass.getName();
+        return name.startsWith("java.io.")
+                || name.startsWith("java.util.");
+    }
+
+    private static Set<String> skip= new HashSet<String>();
+    static {
+        skip.add("java.lang.ThreadLocal$ThreadLocalMap");
+        skip.add("java.lang.ref.Reference");
+        skip.add("java.lang.Boolean");
+        skip.add("java.lang.Class");
+        skip.add("java.security.PermissionCollection");
+        skip.add("java.lang.ArrayStoreException");
+    }
+
 
 }
